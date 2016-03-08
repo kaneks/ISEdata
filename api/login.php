37e59411ca42bae2ec8a731f54777a3fab21cf48
sasinn98 <sasinn98@gmail.com>
2016-03-08 23:57:55 +0700

    rearranged folders

--- api/login.php
+++ api/login.php
@@ -5,9 +5,13 @@
  * Date: 08/03/2016
  * Time: 23:23
  */
+
+require_once("/classes/Database.php");
+require_once("/classes/ISEDatabase.php");
+
 echo "You have entered ".$_POST["email"]." as your email.";
 echo "You have entered ".$_POST["password"]." as your password.";
 $db = new ISEDatabase();
-ISEDatabase::getInstance()->register($_POST["email"], $_POST["password"]);
+$db->register($_POST["email"], $_POST["password"]);
 
 ?>
