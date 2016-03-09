c9c43eb5128a0c8220024323ad75accac111c9de
Kaneks <kanek.eks@gmail.com>
2016-03-09 17:35:42 +0700

    Database

--- api/login.php
+++ api/login.php
@@ -12,6 +12,6 @@ require_once("/classes/ISEDatabase.php");
 echo "You have entered ".$_POST["email"]." as your email.";
 echo "You have entered ".$_POST["password"]." as your password.";
 $db = new ISEDatabase();
-$db->register($_POST["email"], $_POST["password"]);
+$db->login($_POST["email"], $_POST["password"]);
 
 ?>
