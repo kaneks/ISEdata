ab63bde252dd47b1f1a2dd601b5be3afd344214d
Kaneks <kanek.eks@gmail.com>
2016-03-21 14:21:44 +0700

    new rank table fix

---
+++ api/clogin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sasinn
+ * Date: 08/03/2016
+ * Time: 23:23
+ */
+
+require_once("curl.php");
+
+$params = array(
+    "email" => $_POST["email"],
+    "password" => $_POST["password"],
+);
+
+echo httpPost("http://localhost:85/ISEdata/api/login.php",$params);
+
+?>
--- api/login.php
+++ api/login.php
@@ -2,17 +2,15 @@
 /**
  * Created by PhpStorm.
  * User: Sasinn
- * Date: 08/03/2016
- * Time: 23:23
+ * Date: 18/03/2016
+ * Time: 16:02
  */
+require_once("classes/Database.php");
+require_once("classes/ISEDatabase.php");
+#echo "You have entered ".$_POST["email"]." as your email.";
+#echo "You have entered ".$_POST["password"]." as your password.";
+$db = new ISEDatabase();
+return $db->login($_POST["email"], $_POST["password"]);
 
-require_once("curl.php");
-
-$params = array(
-    "email" => $_POST["email"],
-    "password" => $_POST["password"],
-);
-
-echo httpPost("http://localhost:85/ISEdata/api/curlLogin.php",$params);
 
 ?>
